aa466cea52983bce6f44915057b067f89ba1f317
Jose Cueva <josemcz2211@gmail.com>
2025-09-27 20:34:09 -0500

    feat: reemplazar axios por instancia api para autenticación en login y registro

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,7 +1,11 @@
-import axios from 'axios';
-const baseURL = import.meta.env.VITE_API_URL;
+import api from "./api";
 
 export async function login(email: string, password: string) {
-    const response = await axios.post(`${baseURL}/auth/login`, { email, password });
+    const response = await api.post("/auth/login", { email, password });
+    return response.data.data.token; // solo el token
+}
+
+export async function register(nombre: string, email: string, password: string, rol: string) {
+    const response = await api.post("/auth/register", { nombre, email, password, rol });
     return response.data;
 }
