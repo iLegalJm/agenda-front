e27c9c203a550ef63bcf81a96b0df6431ea96524
Jose Cueva <josemcz2211@gmail.com>
2025-10-04 10:29:07 -0500

    feat: eliminar componentes de Citas, Dashboard, Medicos y Pacientes

--- src/pages/Citas.tsx
+++
@@ -1,3 +0,0 @@
-export default function Citas() {
-    return <h1 className="text-2xl font-bold">Citas</h1>;
-}
--- src/pages/Pacientes.tsx
+++
@@ -1,3 +0,0 @@
-export default function Pacientes() {
-    return <h1 className="text-2xl font-bold">Pacientes</h1>;
-}
